5d30cc24eef4fe5648012adbf6d804c46d795132
wangsu2000 <56395301+wangsu2000@users.noreply.github.com>
2020-07-14 11:44:00 +0800

    Update lms.h

--- lms.h
+++ lms.h
@@ -8,13 +8,20 @@ class lms
         int brunch;
         int disclass;
         char* roadname;
+
     }LandMark,*LM;
 public:
+    typedef  int (*FP)(LM a, LM b);  //定义类型
     lms();
     ~lms();
     void print();
+    void sift(LM*r,int length);//堆调整
+    void heapsort(LM*r,int length);//堆排序
     int length;
     LM* base;
+    int cmp1(LM p,LM q);//按照LinkId比较
+     int cmp2(LM p,LM q);//按照disclass比较
+     FP fp[2];
 };
 
 #endif // LMS_H
