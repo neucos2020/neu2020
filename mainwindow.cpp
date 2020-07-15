9210a2e642c94e3255f6d27ef320a776b90d9615
wangsu2000 <56395301+wangsu2000@users.noreply.github.com>
2020-07-15 23:04:57 +0800

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include<qDebug>
-#include<stdio.h>
+#include<iostream>
+#include<string>
+using namespace std;
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -13,6 +15,36 @@ MainWindow::MainWindow(QWidget *parent) :
 
     connect(ui->upPushButton, SIGNAL(clicked()), this, SLOT(upBtnClicked()));
     connect(ui->downPushButton, SIGNAL(clicked()), this, SLOT(downBtnClicked()));
+    ui->cbox_Sort->setSizeAdjustPolicy(QComboBox::AdjustToContents);
+    ui->cbox_Sort->addItem(QString("-请选择-"));
+ui->cbox_Sort->addItem(QString("选择排序"));
+ui->cbox_Sort->addItem(QString("基数排序"));
+ui->cbox_Sort->addItem(QString("堆排序"));
+ui->cbox_Sort->addItem(QString("快速排序"));
+ui->cbox_Sort->addItem(QString("插入排序"));
+ connect(ui->cbox_Sort, SIGNAL(currentIndexChanged(int)), this, SLOT(Sort_method_selected()));
+ ui->cbox_Sort_info->setSizeAdjustPolicy(QComboBox::AdjustToContents);
+ ui->cbox_Sort_info->addItem(QString("-请选择-"));
+ui->cbox_Sort_info->addItem(QString("LinkId"));
+ui->cbox_Sort_info->addItem(QString("flag"));
+ui->cbox_Sort_info->addItem(QString("branch"));
+ui->cbox_Sort_info->addItem(QString("disclass"));
+connect(ui->cbox_Sort_info, SIGNAL(currentIndexChanged(int)), this, SLOT(Sort_info_selected()));
+ui->cbox_Find->setSizeAdjustPolicy(QComboBox::AdjustToContents);
+ui->cbox_Find->addItem(QString("-请选择-"));
+ui->cbox_Find->addItem(QString("-顺序查找-"));
+ui->cbox_Find->addItem(QString("-折半查找-"));
+ui->cbox_Find->addItem(QString("-分块查找-"));
+connect(ui->cbox_Find, SIGNAL(currentIndexChanged(int)), this, SLOT(Find_method()));
+ui->cbox_Find_txt->setSizeAdjustPolicy(QComboBox::AdjustToContents);
+ui->cbox_Find_txt->addItem(QString("-请选择-"));
+ui->cbox_Find_txt->addItem(QString("LinkId"));
+ui->cbox_Find_txt->addItem(QString("flag"));
+ui->cbox_Find_txt->addItem(QString("branch"));
+ui->cbox_Find_txt->addItem(QString("disclass"));
+ui->cbox_Find_txt->addItem(QString("roadname"));
+connect(ui->cbox_Find_txt, SIGNAL(currentIndexChanged(int)), this, SLOT(Find_info()));
+connect(ui->Txt_find, SIGNAL(editingFinished()), this, SLOT(Find_txt()));
 }
 
 MainWindow::~MainWindow()
@@ -97,4 +129,24 @@ void MainWindow::downBtnClicked()
         return;
 
     setListWidget(m_currentPageNum + 1);
+}
+void MainWindow::Sort_method_selected()
+{
+    //printf("%d\n",ui->cbox_Sort->currentIndex());
+}
+void MainWindow::Sort_info_selected(){
+    //printf("%d\n",ui->cbox_Sort_info->currentIndex());
+}
+void MainWindow::Find_method(){
+
+         //    printf("%d\n",ui->cbox_Find->currentIndex());
+}
+void MainWindow::Find_info()
+{
+       // printf("%d\n",ui->cbox_Find_txt->currentIndex());
+}
+void MainWindow::Find_txt(){
+
+string s=ui->Txt_find->text().toStdString();
+cout<<s<<endl;
 }
