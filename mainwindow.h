2615992be35accc67c4bbe52433336d0c68070ee
wangsu2000 <56395301+wangsu2000@users.noreply.github.com>
2020-07-16 15:53:09 +0800

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,8 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include <QMainWindow>
-
+#include<lms.h>
 namespace Ui {
 class MainWindow;
 }
@@ -25,12 +24,18 @@ void Sort_info_selected();
 void Find_method();
 void Find_info();
 void Find_txt();
+void getItem();
+void inputregex();
 private:
     Ui::MainWindow *ui;
     int m_pageCount;
 
     int m_currentPageNum;   //当前页数
     int m_countPageNum;        //总页数
+    int focusedrow;
+    int focusedcol;
+    lms ls;
 };
 
 #endif // MAINWINDOW_H
+
