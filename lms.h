a4ccad6f4db3c9a2e2cafdbe50d15f7303c641de
wangsu2000 <56395301+wangsu2000@users.noreply.github.com>
2020-07-18 21:25:49 +0800

    Update lms.h

--- lms.h
+++ lms.h
@@ -14,9 +14,16 @@ public:
     lms();
     ~lms();
     void print();
-    void sift(LM*r,int length,int info);//堆调整:info是第几个判据
-    void heapsort(LM*r,int length,int info);//堆排序:info是第几个判据
+    void heapInsert(LM*r,int length,int info);//建大根堆
+    void heapify(LM*r,int index,int size,int info);//堆调整:info是第几个判据
+    void heapSort(LM*r,int length,int info);//堆排序:info是第几个判据
+    void shellsort(LM*r,int length,int info);//希尔排序:info是第几个判据
+    int Partition(LM*r,int start,int end,int info);//快速排序分隔
+    void bubbleSort(LM* r,int length,int info);//冒泡排序
+    void selectionSort(LM* r,int length,int info);//选择排序
+    void QuickSort(LM*r,int start,int end,int info);//快速排序:info是第几个判据:注意调用时QuickSort(LM,0,lms.length-1,info);
     int length;
+    int origin_length;//存放base的内存长度;
     LM* base;
     int cmp(LM p,LM q,int info);//按照LinkId比较
 };
